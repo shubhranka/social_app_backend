5d03311214ae34eac016557da9637ae62b6ed4b9
Shubhranka Varma <shubhrankavarma@gmail.com>
2022-12-07 00:07:10 +0530

    added error handler for all routes

--- app.js
+++ app.js
@@ -50,3 +50,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+  });
