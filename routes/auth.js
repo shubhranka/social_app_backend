e95fef02a7f3626aac3e273ff03b6b712b392f7b
Shubhranka Varma <shubhrankavarma@gmail.com>
2022-12-07 14:38:23 +0530

    added signup validations

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,26 @@ const router = express.Router();
 
 router.put(
   '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
   authController.signup
 );
 
