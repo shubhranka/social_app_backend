6fba280e74cf93fca40ece7d0f31122fe37a1408
Shubhranka Varma <shubhrankavarma@gmail.com>
2022-12-07 17:34:34 +0530

    added feed controller - get posts

---
+++ controllers/feed.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const { validationResult } = require('express-validator/check');
+
+const io = require('../socket');
+const Post = require('../models/post');
+const User = require('../models/user');
+
+exports.getPosts = async (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .populate('creator')
+      .sort({ createdAt: -1 })
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200).json({
+      message: 'Fetched posts successfully.',
+      posts: posts,
+      totalItems: totalItems
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const { body } = require('express-validator/check');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
 // GET /feed/posts
 router.get('/posts', isAuth, feedController.getPosts);
 
